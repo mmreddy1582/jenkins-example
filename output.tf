58a8af7337f53c1f32cacf5b7d9040f923f7d1d0
mmreddy1582 <35883559+mmreddy1582@users.noreply.github.com>
2019-12-28 01:36:48 +0800

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
 output "instance_ips" {
   value = ["${aws_instance.example.*.public_ip}"]
   value = ["${aws_instance.example.*.private_ip}"]
-  description = "The private and public IP address of the main server instance."
+  description = "The private and public IP address of the main server instance.After enabling webhook"
 }
